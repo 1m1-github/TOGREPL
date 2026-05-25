77b6208b319ba4977100fa58000e614ce126bfd1
1m1 <imi@1m1.io>
2026-05-24 22:40:25 -0500

    1.0.0

---
+++ src/TOGREPL.jl
@@ -0,0 +1,35 @@
+module TOGREPL
+
+using REPL, ReplMaker
+using LoopOS: listen, InputPeripheral, OutputPeripheral
+import Base.take!, Base.put!
+
+struct REPLInput <: InputPeripheral
+    c::Channel{String}
+end
+take!(::REPLInput) = take!(REPL.c)
+state(::REPLInput) = "TOGREPL.REPL"
+const REPLINPUT = REPLInput(Channel{String}(Inf))
+
+struct REPLOutput <: OutputPeripheral end
+put!(::REPLOutput, a) = println(stdout, a)
+
+repl_parse(s) = put!(REPL.c, string(strip("""$s""")))
+
+function awaken(GOD)
+    listen(REPLINPUT)
+    term = REPL.Terminals.TTYTerminal("tog", stdin, stdout, stderr)
+    repl = LineEditREPL(term, true, true)
+    ReplMaker.initrepl(
+        repl_parse,
+        repl=repl,
+        prompt_text="> ",
+        prompt_color=:light_cyan,
+        start_key="\\C-G",
+        mode_name="GOD",
+    )
+    GOD && write(stdin.buffer, "\x07")
+    REPL.run_repl(repl)
+end
+
+end
