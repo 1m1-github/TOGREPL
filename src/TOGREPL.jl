8589e7d5a8d0e1a0f680d0ade3ca537c8a3f14a0
1m1 <imi@1m1.io>
2026-06-05 13:18:55 -0500

    9.0.0

--- src/TOGREPL.jl
+++ src/TOGREPL.jl
@@ -9,10 +9,16 @@ struct repl <: Peripheral
     # r::AbstractREPL
     c::Channel{String}
 end
-take!(::repl) = take!(REPLINSTANCE.c)
-put!(::repl, a) = println(stdout, a)
-state(::repl) = "TOGREPL.REPL"
 const REPLINSTANCE = repl(Channel{String}(Inf))
+take!(::repl) = begin
+    @show "TOGREPL.take!", REPLINSTANCE.c
+    take!(REPLINSTANCE.c)
+end
+put!(::repl, a) = begin
+    @show "TOGREPL.put!", a
+    println(stdout, a)
+end
+state(::repl) = "TOGREPL.REPL"
 
 # old code
 # struct REPLInput <: Peripheral
@@ -24,25 +30,30 @@ const REPLINSTANCE = repl(Channel{String}(Inf))
 # put!(::REPLOutput, a) = println(stdout, a)
 # old code
 
-repl_parse(s) = put!(REPLINSTANCE.c, string(strip("""$s""")))
+repl_parse(s) = begin
+    @show "TOGREPL.repl_parse", s
+    put!(REPLINSTANCE.c, string(strip("""$s""")))
+end
 # repl_parse(s) = println(s)
 
 # function awaken(GOD)
 function awaken()
     # term = REPL.Terminals.TTYTerminal("tog", stdin, stdout, stderr)
     # REPLINSTANCE[] = repl(LineEditREPL(term, true, true))
-    atreplinit(repl ->
-        REPLINSTANCE[] = (ReplMaker.initrepl(
+    atreplinit(repl -> begin
+        ReplMaker.initrepl(
             repl_parse,
             repl=repl,
             prompt_text="> ",
             prompt_color=:light_cyan,
             start_key="\\C-G", # "\x07",
             mode_name="GOD",
-        )))
+        )
+        listen(REPLINSTANCE)
+    end
+    )
     # write(stdin.buffer, "\x07")
     # GOD && write(stdin.buffer, "\x07")
-    # listen(REPLINSTANCE[])
     # REPL.run_repl(REPLINSTANCE[].r)
 end
 
