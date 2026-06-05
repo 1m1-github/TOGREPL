84c24b78cc2ad8a2b066417a5e839532fc8f8d1b
1m1 <imi@1m1.io>
2026-06-05 11:37:44 -0500

    7.0.0

--- src/TOGREPL.jl
+++ src/TOGREPL.jl
@@ -1,14 +1,15 @@
 module TOGREPL
 
-using REPL, ReplMaker
-using LoopOS: listen, Peripheral
-import Base.take!, Base.put!
+# using REPL
+using ReplMaker
+# using LoopOS: listen, Peripheral
+# import Base.take!, Base.put!
 
-struct repl <: Peripheral r::AbstractREPL end
-take!(::repl) = take!(REPL.c)
-put!(::repl, a) = println(stdout, a)
-state(::repl) = "TOGREPL.REPL"
-const REPLINSTANCE = Ref{repl}()
+# struct repl <: Peripheral r::AbstractREPL end
+# take!(::repl) = take!(REPL.c)
+# put!(::repl, a) = println(stdout, a)
+# state(::repl) = "TOGREPL.REPL"
+# const REPLINSTANCE = Ref{repl}()
 
 # old code
 # struct REPLInput <: Peripheral
@@ -20,24 +21,25 @@ const REPLINSTANCE = Ref{repl}()
 # put!(::REPLOutput, a) = println(stdout, a)
 # old code
 
-repl_parse(s) = put!(REPL.c, string(strip("""$s""")))
+# repl_parse(s) = put!(REPL.c, string(strip("""$s""")))
+repl_parse(s) = println(s)
 
 # function awaken(GOD)
 function awaken()
-    term = REPL.Terminals.TTYTerminal("tog", stdin, stdout, stderr)
-    REPLINSTANCE[] = repl(LineEditREPL(term, true, true))
+    # term = REPL.Terminals.TTYTerminal("tog", stdin, stdout, stderr)
+    # REPLINSTANCE[] = repl(LineEditREPL(term, true, true))
     ReplMaker.initrepl(
         repl_parse,
-        repl=REPLINSTANCE[].r,
+        # repl=REPLINSTANCE[].r,
         prompt_text="> ",
         prompt_color=:light_cyan,
         start_key="\\C-G", # "\x07",
         mode_name="GOD",
     )
-    write(stdin.buffer, "\x07")
+    # write(stdin.buffer, "\x07")
     # GOD && write(stdin.buffer, "\x07")
-    listen(REPLINSTANCE[])
-    REPL.run_repl(REPLINSTANCE[].r)
+    # listen(REPLINSTANCE[])
+    # REPL.run_repl(REPLINSTANCE[].r)
 end
 
 end
